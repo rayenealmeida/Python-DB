c046694726636df832a083779fdd686a0346970b
rayenealmeida <almeidarayene@gmail.com>
2025-02-05 23:39:30 -0300

    DELETE

--- conexao_db.py
+++ conexao_db.py
@@ -9,5 +9,5 @@ cursor = conexao.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, Nome VARCHAR(100), email VARCHAR(150))')
 
 data = ("Joana", "jota@gmail.com")
-cursor.execute("INSERT INTO clientes(nome, email) VALUES(?,?);", data)
+cursor.execute("DELETE INTO clientes(nome, email) VALUES(?,?);", data)
 conexao.commit()
