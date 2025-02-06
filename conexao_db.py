09bcee4ec13ca029a61c22cee7a6e63fc4e858fe
rayenealmeida <almeidarayene@gmail.com>
2025-02-05 23:12:45 -0300

    first commit

---
+++ conexao_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+from pathlib import Path 
+
+ROOT_PATH =Path(__file__).parent
+
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.sqlite")
+cursor = conexao.cursor()
+
+cursor.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUOINCREMENT, Nome VARCHAR(100), email VARCHAR(150))')
+
+cursor.execute('INSERT INTO clientes()')
