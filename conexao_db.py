abba0622048d464e6a6b48965dee29a3a97df7ab
rayenealmeida <almeidarayene@gmail.com>
2025-02-05 23:35:38 -0300

    atributos

--- conexao_db.py
+++ conexao_db.py
@@ -8,5 +8,6 @@ cursor = conexao.cursor()
 
 cursor.execute('CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, Nome VARCHAR(100), email VARCHAR(150))')
 
-data= ("Maria", "maria@gmail.com")
+data = ("Joana", "jota@gmail.com")
 cursor.execute("INSERT INTO clientes(nome, email) VALUES(?,?);", data)
+conexao.commit()
