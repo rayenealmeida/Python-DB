92d86d7be09978eb24d38934ef0b1e9f695acbf7
rayenealmeida <almeidarayene@gmail.com>
2025-02-07 23:32:19 -0300

    atualizar registro

--- conexao_db.py
+++ conexao_db.py
@@ -6,8 +6,22 @@ ROOT_PATH =Path(__file__).parent
 conexao = sqlite3.connect(ROOT_PATH / "meu_banco.sqlite")
 cursor = conexao.cursor()
 
-cursor.execute('CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, Nome VARCHAR(100), email VARCHAR(150))')
+def criar_tabela(conexao, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, Nome VARCHAR(100), email VARCHAR(150))"
+    )
+    conexao.commit()
 
-data = ("Maria", "maria@gmail.com")
-cursor.execute("DELETE INTO clientes(nome, email) VALUES(?,?);", data)
-conexao.commit()
+# Inserir Registro
+def inserir_registro(conexao, cursor, Nome, email):
+    data = ("Maria", "maria@gmail.com")
+    cursor.execute("INSERT INTO clientes(nome, email) VALUES(?,?);", data)
+    conexao.commit()
+
+# Atualizar Registro
+def atualizar_registro(conexao, cursor, Nome, email, id):
+    data = (Nome, email, id)
+    cursor.execute("UPDATE clientes SET Nome=?, email=? WHERE id=?;", data)
+    conexao.commit()
+
+atualizar_registro(conexao, cursor, 'Maria Aparecida', 'mariaApe@gmail.com')
